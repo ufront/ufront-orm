a1117db8c56a6bf6281b358d8a8e395bfbba6641
Jason O'Neil <jason.oneil@gmail.com>
2014-11-06 15:58:34 +0800

    Updated test script to run (Neko/PHP) and (MySQL/Sqlite)
    And also to compile with the compilation server, to test those.
    
    Currently the validate() tests are still failing on the 2nd (or more)
    compilation with the server.

--- testAll.sh
+++ testAll.sh
@@ -1,5 +1,10 @@
-echo "Compile"
-haxe test.hxml || exit
+echo "Start Haxe server"
+haxe --wait 6123 &
+
+sleep 1
+
+echo "Compile #1"
+haxe --connect 6123 test.hxml || exit
 
 echo "Test neko mysql"
 neko build/neko_test.n mysql || exit
@@ -10,3 +15,11 @@ echo "Test PHP mysql"
 php build/php_test.php mysql || exit
 echo "Test PHP sqlite"
 php build/php_test.php sqlite || exit
+
+echo "Compile #2 (using cache)"
+haxe --connect 6123 test.hxml || exit
+
+echo "Re-test neko mysql after compile using cache"
+neko build/neko_test.n mysql || exit
+
+echo "Finished! All tests passed"
