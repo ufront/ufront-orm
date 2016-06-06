eb64d8c2999565d1bef8861f42a76e2546b92631
Jason O'Neil <jason.oneil@gmail.com>
2016-06-06 14:44:08 +0800

    Add whitespace and dividers in unit test output to make it easier to read

--- testAll.sh
+++ testAll.sh
@@ -18,19 +18,28 @@ mkdir -p doc build
 echo "Compile #1"
 haxe --connect $HAXE_COMPILER_PORT test.hxml || exit
 
+echo ""
 echo "Test neko mysql"
+echo "---------------"
 neko build/neko_test.n mysql || exit
+echo ""
 echo "Test neko sqlite"
+echo "----------------"
 neko build/neko_test.n sqlite #|| exit
 
+echo ""
 echo "Test PHP mysql"
+echo "--------------"
 php build/php_test.php mysql || exit
+echo ""
 echo "Test PHP sqlite"
+echo "---------------"
 php build/php_test.php sqlite || exit
 
 echo "Compile #2 (using cache)"
 haxe --connect $HAXE_COMPILER_PORT test.hxml || exit
 
+echo "--------------------------------------------"
 echo "Re-test neko mysql after compile using cache"
 neko build/neko_test.n mysql || exit
 
