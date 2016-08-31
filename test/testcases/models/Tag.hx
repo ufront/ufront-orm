7c4cd75141f4ec5d4677854a833c08cc47a4728f
Jason O'Neil <jason.oneil@gmail.com>
2016-09-01 08:50:57 +1000

    Fix: tags can be more than 10 characters.
    New versions of MySQL actually enforce the length limit,
    and our tests save longer tags than 10 characters, so we
    had exceptions being thrown.
    
    Easier to expand the length than to adjust the tests to
    use shorter tags.

--- test/testcases/models/Tag.hx
+++ test/testcases/models/Tag.hx
@@ -7,7 +7,7 @@ import sys.db.Types;
 class Tag extends Object {
 	@:validate( _.length>3, "Your tag url must be at least 3 letters long" )
 	@:validate( ~/^[a-z0-9_]+$/.match(_), "Your tag url must only use a-z, 0-9 and underscores" )
-	public var url:SString<10>;
-	
+	public var url:SString<255>;
+
 	public var posts:ManyToMany<Tag,BlogPost>;
 }
